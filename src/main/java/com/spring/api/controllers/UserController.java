1d83e8e1efefc0e1518ebda719fd4b63596c3a6a
DimitrisDoxopoulos <olonir@hotmail.com>
2023-11-10 10:01:01 +0200

    Moved CORS on Users from requests to controller

--- src/main/java/com/spring/api/controllers/UserController.java
+++ src/main/java/com/spring/api/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200/")
 public class UserController {
     private final UserRepository userRepository;
 
@@ -15,13 +16,11 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
-    @CrossOrigin(origins = "http://localhost:4200/")
     @PostMapping("/users")
     void addUser(@RequestBody User user) {
         userRepository.save(user);
     }
 
-    @CrossOrigin(origins = "http://localhost:4200/")
     @GetMapping("/users")
     public List<User> getUsers() {
         return (List<User>) userRepository.findAll();
