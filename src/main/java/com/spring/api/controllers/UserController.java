dbf4fcd072eac15e15d1f7a85a11e37e1a85d0b4
DimitrisDoxopoulos <olonir@hotmail.com>
2023-11-10 09:57:07 +0200

    Added CORS on user controller

--- src/main/java/com/spring/api/controllers/UserController.java
+++ src/main/java/com/spring/api/controllers/UserController.java
@@ -2,10 +2,8 @@ package com.spring.api.controllers;
 
 import com.spring.api.models.User;
 import com.spring.api.repositories.UserRepository;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -17,11 +15,13 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
+    @CrossOrigin(origins = "http://localhost:4200/")
     @PostMapping("/users")
     void addUser(@RequestBody User user) {
         userRepository.save(user);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200/")
     @GetMapping("/users")
     public List<User> getUsers() {
         return (List<User>) userRepository.findAll();
